4f5de95b0c4feec841608cf2d4050058213e5a3d
John Krukar <john.a.krukar@gmail.com>
2018-05-07 08:35:52 -0600

    Add files via upload

---
+++ trainingSet.bat
@@ -0,0 +1,156 @@
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_60fov_1v50.exe --run-id=60fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_90fov_1v50.exe --run-id=90fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_120fov_1v50.exe --run-id=120fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_150fov_1v50.exe --run-id=150fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_180fov_1v50.exe --run-id=180fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_210fov_1v50.exe --run-id=210fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_240fov_1v50.exe --run-id=240fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_270fov_1v50.exe --run-id=270fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_300fov_1v50.exe --run-id=300fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_330fov_1v50.exe --run-id=330fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+rem Activate Anaconda and ml-agents environment
+call C:\Users\johna\Anaconda3\Scripts\activate.bat
+call activate ml-agents
+cd predatorprey
+
+rem Train next model
+call python python/learn.py final_360fov_1v50.exe --run-id=360fov_1 --train
+
+rem Deactivate Anaconda and ml-agents environment
+call deactivate
+call deactivate
+cd ..
+rem ----------------------------------------------------------------------------------------------------------------------------------------------------
+
+
